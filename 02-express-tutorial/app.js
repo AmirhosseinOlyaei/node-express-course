2c30ecc95e699c8b0888bb5f6b5fdcb55d85deed
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-03-18 18:46:26 -0400

    Initialized basic Express server setup
    Expanded the initial app skeleton to include setup of a basic Express server. Installed middleware to serve static files from the 'public' directory and implemented rudimentary GET and POST routes for the root path. Added a generic handler for unmatched routes to return a 'Page not found' message. Server now listens on port 3000, ready for further route development and middleware integration.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,28 @@
-console.log('Express Tutorial')
+console.log("Express Tutorial");
+
+// The require statement to import the express module
+const express = require("express");
+
+// Creation of the app as returned from calling express()
+const app = express();
+
+// app.use statements for the middleware. You’ll eventually use many kinds of middleware, but for now the only middleware we are using is express.static().
+app.use(express.static("./public"));
+
+// Listen on port 3000
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
+
+// app.get and app.post statements for the routes you will handle. Eventually these will be refactored into router modules, but for now you can put them inline.
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.post("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// An app.all statement after these to handle page not found conditions.
+app.all("*", (req, res) => {
+  res.send("Page not found");
+});
