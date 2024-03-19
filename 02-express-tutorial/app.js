19bda16fe5e9f6d0ffe08f0bb71dbb72cc074401
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-03-19 00:03:51 -0400

    ``` Expand product API and add client-side fetching
    Enhanced the Express app with new product-focused endpoints:
    - Added product retrieval by ID
    - Implemented product search and limit query functionality
    - Created an endpoint to fetch products under a given price
    
    Updated the front-end to interact with the new API features, allowing users to:
    - Fetch all products or a single product by ID
    - Perform a search based on a query with optional result limit
    - Get products below a specified price point
    
    These changes improve the overall functionality of our application by allowing more dynamic data retrieval and better user interactions on the client side.
    ```

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,4 +1,5 @@
 console.log("Express Tutorial");
+const { products } = require("./data");
 
 // The require statement to import the express module
 const express = require("express");
@@ -15,13 +16,55 @@ app.listen(3000, () => {
 });
 
 // app.get and app.post statements for the routes you will handle. Eventually these will be refactored into router modules, but for now you can put them inline.
-app.get("/api/v1/test", (req, res) => {
-  // res.send("Getting ...");
-  res.json({ message: "It worked!" });
+app.get("/get", (req, res) => {
+  res.send("Getting ...");
 });
 app.post("/post", (req, res) => {
-  console.log(req.body);
   res.send("Posting!");
+});
+app.get("/api/v1/test", (req, res) => {
+  res.json({ message: "It worked!" });
+});
+app.get("/api/v1/products", (req, res) => {
+  res.json(products);
+});
+
+// http://localhost:3000/api/v1/products/1
+app.get("/api/v1/products/:productID", (req, res) => {
+  const idToFind = parseInt(req.params.productID);
+  const product = products.find((p) => p.id === idToFind);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: "That product was not found." });
+  }
+});
+
+// http://localhost:3000/api/v1/query?search=a&limit=2
+app.get("/api/v1/query", (req, res) => {
+  const { search, limit } = req.query;
+  let filteredProducts = products;
+
+  if (search) {
+    filteredProducts = filteredProducts.filter((p) =>
+      p.name.startsWith(search)
+    );
+  }
+
+  if (limit) {
+    filteredProducts = filteredProducts.slice(0, parseInt(limit));
+  }
+
+  res.json(filteredProducts);
+});
+
+// http://localhost:3000/api/v1/products/under/30
+app.get("/api/v1/products/under/:price", (req, res) => {
+  const maxPrice = parseFloat(req.params.price);
+  const filteredProducts = products.filter((p) => p.price <= maxPrice);
+
+  res.json(filteredProducts);
 });
 
 // An app.all statement after these to handle page not found conditions.
