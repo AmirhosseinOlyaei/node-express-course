88e0af1c1910ea55b198ddea5b60cbc5af1c11c4
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-04-05 18:55:00 -0400

    "Update task controller to return updated task and handle 404 if task not found"
    This commit updates the `updateTask` controller function to find a task by its ID and update it with the provided request body. If the task is not found, it returns a 404 status code with an error message. If there is an error during the update process, it returns a 500 status code with the error message.

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -34,10 +34,19 @@ const getTask = async (req, res) => {
 
 const updateTask = async (req, res) => {
   try {
-    res.send("update task");
+    const { id: taskID } = req.params;
+
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task)
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+
+    res.status(200).json({ task });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ msg: error.message });
   }
 };
 
