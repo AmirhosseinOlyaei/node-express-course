22aa52c5af22bbe496f77657fbe4b7da078d09c1
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-04-05 22:31:19 -0400

    Add error handler middleware
    - Update app.js
    - Add require for error-handler middleware
    - Add errorHandlerMiddleware to the middleware stack after notFound
    - No functional changes to existing routes or functionality

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
 app.use(express.static("./public"));
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
