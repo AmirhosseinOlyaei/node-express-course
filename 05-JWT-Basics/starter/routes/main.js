52bbb5feb7b0ce7fe77b0440d8025cf245fc6b06
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-05-16 20:52:48 -0400

    "Added authentication middleware and error handler, and updated main routes to include auth middleware"

--- 05-JWT-Basics/starter/routes/main.js
+++ 05-JWT-Basics/starter/routes/main.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const { login, dashboard } = require("../controllers/main");
 
+const authMiddleware = require("../middleware/auth");
+
 router.route("/dashboard").get(dashboard);
 router.route("/login").post(login);
 
