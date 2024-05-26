e5619fc3f4392d30557a6c7ed0cafee1dffd5081
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-05-25 22:51:17 -0400

    "Removed unnecessary comments"

--- 06-jobs-api/starter/routes/jobs.js
+++ 06-jobs-api/starter/routes/jobs.js
@@ -1,10 +1,7 @@
 // 06-jobs-api/starter/routes/jobs.js
-//
-
-"use strict";
-
 const express = require("express");
 const router = express.Router();
+
 const {
   createJob,
   deleteJob,
@@ -12,10 +9,13 @@ const {
   updateJob,
   getJob,
 } = require("../controllers/jobs");
+
 const { checkToken } = require("../middlewares/auth");
+
 router.post("/", checkToken, createJob);
 router.get("/", checkToken, getAllJobs);
 router.get("/:id", checkToken, getJob);
 router.delete("/:id", checkToken, deleteJob);
 router.patch("/:id", checkToken, updateJob);
+
 module.exports = router;
